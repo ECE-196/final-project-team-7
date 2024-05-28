aeae40e45cb16ec08ebed7863ab7f8c732072306
qianyunwang <qianyunjean@126.com>
2024-05-27 17:30:22 -0700

    Co-authored-by: yukthadeesan <yukthadeesan@users.noreply.github.com> Co-authored-by: matty-zee <matty-zee@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,11 +5,20 @@ from tkinter.tix import MAIN
 from typing import Final
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ConversationHandler, ContextTypes 
+import cv2
+import pickle
+import face_recognition
+from imutils.video import VideoStream
+from imutils.video import FPS
+import imutils
+import time
+from imutils import paths
 
 
 TOKEN: Final = '6635383068:AAFJituA5_o8o7L_ypkBuc0YgQlggetRee8'
 BOT_USERNAME: Final = '@facial_recognizer_bot'
 
+flag = False
 # recognized_faces = {} 
 
 # Function to generate the file path for a user's data based on their user ID
@@ -51,6 +60,112 @@ async def name(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'Got it! Please send me a video of {name}.')
     return VIDEO
 
+async def open_cam(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Camera on. Send `/stop_cam` to stop.')
+    #Initialize 'currentname' to trigger only when a new person is identified.
+    currentname = "unknown"
+    #Determine faces from encodings.pickle file model created from train_model.py
+    encodingsP = "encodings.pickle"
+
+    # load the known faces and embeddings along with OpenCV's Haar
+    # cascade for face detection
+    print("[INFO] loading encodings + face detector...")
+    data = pickle.loads(open(encodingsP, "rb").read())
+
+    # initialize the video stream and allow the camera sensor to warm up
+    # Set the ser to the followng
+    # src = 0 : for the build in single web cam, could be your laptop webcam
+    # src = 2 : I had to set it to 2 inorder to use the USB webcam attached to my laptop
+    vs = VideoStream(src=0,framerate=10).start()
+    #vs = VideoStream(usePiCamera=True).start()
+    time.sleep(2.0)
+
+    # start the FPS counter
+    fps = FPS().start()
+    
+    # loop over frames from the video file stream
+    while True:
+        if flag:
+            break
+        # grab the frame from the threaded video stream and resize it
+        # to 500px (to speedup processing)
+        frame = vs.read()
+        frame = imutils.resize(frame, width=500)
+        # Detect the fce boxes
+        boxes = face_recognition.face_locations(frame)
+        # compute the facial embeddings for each face bounding box
+        encodings = face_recognition.face_encodings(frame, boxes)
+        names = []
+
+        # loop over the facial embeddings
+        for encoding in encodings:
+            # attempt to match each face in the input image to our known
+            # encodings
+            matches = face_recognition.compare_faces(data["encodings"],
+                encoding)
+            name = "Unknown" #if face is not recognized, then print Unknown
+
+            # check to see if we have found a match
+            if True in matches:
+                # find the indexes of all matched faces then initialize a
+                # dictionary to count the total number of times each face
+                # was matched
+                matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+                counts = {}
+
+                # loop over the matched indexes and maintain a count for
+                # each recognized face face
+                for i in matchedIdxs:
+                    name = data["names"][i]
+                    counts[name] = counts.get(name, 0) + 1
+
+                # determine the recognized face with the largest number
+                # of votes (note: in the event of an unlikely tie Python
+                # will select first entry in the dictionary)
+                name = max(counts, key=counts.get)
+
+                #If someone in your dataset is identified, print their name on the screen
+                if currentname != name:
+                    currentname = name
+                    print(currentname)
+
+            # update the list of names
+            names.append(name)
+
+        # loop over the recognized faces
+        for ((top, right, bottom, left), name) in zip(boxes, names):
+            # draw the predicted face name on the image - color is in BGR
+            cv2.rectangle(frame, (left, top), (right, bottom),
+                (0, 255, 225), 2)
+            y = top - 15 if top - 15 > 15 else top + 15
+            cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+                .8, (0, 255, 255), 2)
+
+        # display the image to our screen
+        cv2.imshow("Facial Recognition is Running", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # quit when 'q' key is pressed
+        #if key == ord("q"):
+        #    break
+
+        # update the FPS counter
+        fps.update()
+
+    # stop the timer and display FPS information
+    fps.stop()
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
+    await update.message.reply_text(f'Camera off.')
+
+async def stop_cam(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    flag = True
+    await update.message.reply_text('Stopping camera...')
+
 async def video(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         video_file = await update.message.video.get_file()
@@ -59,9 +174,78 @@ async def video(update: Update, context: ContextTypes.DEFAULT_TYPE):
         video_path = f"{user_id}_{name}_{video_file.file_unique_id}.mp4"
         await video_file.download_to_drive(video_path)
 
+        #make directory for images
+        output_dir = os.path.join('dataset', name)
+        os.makedirs(output_dir, exist_ok=True)
+        
+        vidcap = cv2.VideoCapture(video_path)
+        success,image = vidcap.read()
+        count = 0       
+
+        # number of frames to skip
+        numFrameToSave = 12
+        frame_cnt = 0
+        while success: # check success here might break your program
+            success,image = vidcap.read() #success might be false and image might be None
+            #check success here
+            if not success:
+                break
+
+            # on every numFrameToSave 
+            if (count % numFrameToSave ==0):  
+                frame_path = os.path.join(output_dir, "%s_%d.jpg" % (name, frame_cnt))
+                cv2.imwrite(frame_path, image)
+                frame_cnt += 1  
+
+            if cv2.waitKey(10) == 27:                     
+                break
+            count += 1
         # Add to the dictionary:
         # recognized_faces[name] = video_path  
 
+
+        ## MODEL TRAINING ## 
+        
+        imagePaths = list(paths.list_images(f"dataset/{name}"))        
+        # initialize the list of known encodings and known names
+        knownEncodings = []
+        knownNames = []
+
+        # loop over the image paths
+        for (i, imagePath) in enumerate(imagePaths):
+        # extract the person name from the image path
+            print("[INFO] processing image {}/{}".format(i + 1,
+            len(imagePaths)))
+        name = imagePath.split(os.path.sep)[-2]
+
+        # load the input image and convert it from RGB (OpenCV ordering)
+        # to dlib ordering (RGB)
+        image = cv2.imread(imagePath)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # detect the (x, y)-coordinates of the bounding boxes
+        # corresponding to each face in the input image
+        boxes = face_recognition.face_locations(rgb,
+            model="hog")
+
+        # compute the facial embedding for the face
+        encodings = face_recognition.face_encodings(rgb, boxes)
+
+        # loop over the encodings
+        for encoding in encodings:
+            # add each encoding + name to our set of known names and
+            # encodings
+            knownEncodings.append(encoding)
+            knownNames.append(name)
+
+        # dump the facial encodings + names to disk
+        print("[INFO] serializing encodings...")
+        data = {"encodings": knownEncodings, "names": knownNames}
+        f = open("encodings.pickle", "wb")
+        f.write(pickle.dumps(data))
+        f.close()
+
+        ### END OF MODEL TRAINING ###
         # Load user data
         user_data = context.user_data.get('recognized_faces', {})
         if name in user_data:
@@ -126,6 +310,8 @@ if __name__ == '__main__':
     print('Starting bot...')
     app = Application.builder().token(TOKEN).build()
 
+    
+
     add_face_handler = ConversationHandler(
         entry_points=[CommandHandler('add_face', add_face)],
         states={
@@ -145,6 +331,9 @@ if __name__ == '__main__':
 
     app.add_handler(add_face_handler)
     app.add_handler(remove_face_handler)
+    app.add_handler(CommandHandler('open_cam', open_cam))
+    app.add_handler(CommandHandler('stop_cam', stop_cam))
+
 
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('help', help_command)) 
